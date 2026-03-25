a99fd0bae0fdc9c5f38087a82a4ee594c6ff3973
majinmaru <gotch11@naver.com>
2026-03-25 19:45:55 +0900

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: 'YOUR_SUPABASE_URL',
-  anonKey: 'YOUR_SUPABASE_ANON_KEY',
+  url: 'https://dlybhuneuwukkvyfrmmh.supabase.co',
+  anonKey: 'sb_publishable_DuLGF5xzQ-cYUP-L4kg41A_qW_dbncR',
 };
